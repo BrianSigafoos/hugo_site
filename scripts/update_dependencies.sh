857232db39dcd1ab92eade7b8d7fb355f9368480
Brian Sigafoos <sigafoos@gmail.com>
2022-03-12 07:39:16 -0500

    Update dependencies 2022-03-12 (#35)
    * Update dependencies 2022-03-12
    
    * Add script

---
+++ scripts/update_dependencies.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+PREVIOUS_BRANCH=$(git branch --show-current)
+NEW_BRANCH=update/$(date +%Y%m%d)
+READABLE_DATE=$(date +%Y-%m-%d)
+
+# Stash work, checkout main, and pull latest
+echo "Stashing work, checking out main, pulling latest..."
+git stash push
+git checkout main
+git pull --rebase --autostash
+
+# New branch
+git checkout -b $NEW_BRANCH
+
+echo "Updating dependencies"
+brew bundle
+yarn upgrade --latest
+# Wrapper for:
+#   git submodule foreach --recursive git checkout main
+#   git submodule foreach --recursive git pull origin main
+yarn run submodule:latest
+
+
+echo "Committing changes"
+git commit -a -m "Update dependencies $READABLE_DATE" --no-verify
+git push
+
+echo "Creating PR using GitHub CLI"
+gh pr create --fill
+
+# Back to previous branch
+echo "Checking out previous branch"
+git checkout $PREVIOUS_BRANCH
+echo "If needed, run: git stash pop (gstp)"
