c21b57717231f70bf92d4b28b719c25cab4ef16a
Brian Sigafoos <sigafoos@gmail.com>
2022-06-04 06:42:20 -0400

    Update tailwind config and main.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
 module.exports = {
-  mode: 'jit',
   content: ['layouts/**/*.html', 'content/*.md'],
   safelist: ['gist', 'gist-data', 'gist-file'],
   theme: {
-    extend: {
-      colors: {
-        primary: 'var(--color-primary)'
-      }
-    },
     container: {
       // .container applies these to center and add padding
       center: true, // mx-auto
       padding: '1.5rem' // px-6
+    },
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)'
+      }
     }
   },
-  variants: {},
-  plugins: []
+  // variants: {},
+  // plugins: []
 }
