5747caf9e428ca5ff6ed02c8bdaedcce1a4cc6d1
Brian Sigafoos <sigafoos@gmail.com>
2022-07-23 16:44:15 -0400

    Switch to Tailwind typogrpahy from custom CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,9 +10,31 @@ module.exports = {
     extend: {
       colors: {
         primary: 'var(--color-primary)'
-      }
-    }
+      },
+      typography: ({ theme }) => ({
+        custom: {
+          css: {
+            '--tw-prose-body': 'var(--text-color)',
+            '--tw-prose-headings': 'var(--text-bright)',
+            '--tw-prose-lead': 'var(--text-muted)',
+            '--tw-prose-links': 'var(--color-primary)',
+            '--tw-prose-bold': 'var(--text-bright)',
+            '--tw-prose-counters': 'var(--text-muted)',
+            '--tw-prose-bullets': 'var(--text-muted)',
+            '--tw-prose-hr': 'var(--text-faint)',
+            '--tw-prose-quotes': 'var(--text-bright)',
+            '--tw-prose-quote-borders': 'var(--text-faint)',
+            '--tw-prose-captions': 'var(--text-color)',
+            '--tw-prose-code': 'var(--text-bright)',
+            '--tw-prose-pre-code': 'var(--text-faint)',
+            '--tw-prose-pre-bg': 'var(--bg-bright)',
+          },
+        },
+      }),
+    },
   },
   // variants: {},
-  // plugins: []
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
