68910ed3c101cc4bd61c20f7bd942e66edca449a
Brian Sigafoos <sigafoos@gmail.com>
2022-08-10 06:59:41 -0400

    Document .prose-custom class

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
       colors: {
         primary: 'var(--color-primary)'
       },
+      //Add .prose-custom class to use CSS variables
       typography: ({ theme }) => ({
         custom: {
           css: {
